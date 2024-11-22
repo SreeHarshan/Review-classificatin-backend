ade2c80dcc2b8420d91fcdaf8866d0f393fec4a1
SreeHarshan <sreeerode12@gmail.com>
2024-11-22 20:28:36 +0530

    Merged with new model

--- server.py
+++ server.py
@@ -11,13 +11,11 @@ def classify():
     if request.method == "POST":
         query = request.form.get("query")
         output,confidence_score = clf.predict(query)  
-        return render_template("index.html", query=query, prediction=output, confidence=confidence_score)
-    return render_template("index.html", query=None, prediction=None, confidence=None)
         if(len(query)>150):
             output = clf.predict(query)  
         else:
             output = None
-        return render_template("index.html", query=query, prediction=output)
+        return render_template("index.html", query=query, prediction=output, confidence=confidence_score)
     return render_template("index.html", query=None, prediction=None)
 
 if __name__ == "__main__":
